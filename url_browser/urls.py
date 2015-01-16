81c5ae7165b7e97cc02093421c4300dc228f3fd4
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-01-16 20:03:33 +0100

    initial commit

---
+++ url_browser/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+from views import form_view
+
+
+urlpatterns = patterns('',
+    url(r'^form_view/$', form_view, name='form_view'),
+)
---
+++ url_browser/views.py
@@ -0,0 +1,55 @@
+from django.utils import six
+from django.forms import forms as newforms
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.urlresolvers import get_urlconf, get_resolver
+
+from url_browser import form_registry
+
+
+#load all urlnames
+def form_view(request):
+    apps = {}
+    apps['Other'] = {}
+    media = newforms.Media()
+
+    known_url_names = []
+
+    def _stack_known_urls(reverse_dict, ns=None):
+        for url_name, url_rules in reverse_dict.items():
+            if isinstance(url_name, six.string_types):
+                if ns is not None:
+                    url_name = '%s:%s' % (ns, url_name)
+                known_url_names.append(url_name)
+
+    #urls
+    resolver = get_resolver(get_urlconf())
+    for ns, (url_prefix, ns_resolver) in resolver.namespace_dict.items():
+        if ns != 'admin':
+            _stack_known_urls(ns_resolver.reverse_dict, ns)
+    _stack_known_urls(resolver.reverse_dict)
+
+    if request.method == 'POST':
+        for app, forms in form_registry.get_apps().items():
+            apps[app] = {}
+            for url_name, form in forms.items():
+                apps[app][request.POST.get('url_name')] = form(url_name, request.POST)
+                if apps[app][request.POST.get('url_name')].is_valid():
+                    if request.is_ajax():
+                        return HttpResponse(apps[app][request.POST.get('url_name')].submit(), content_type="text/plain")
+    else:
+        print form_registry.get_apps().items()
+        for app, forms in form_registry.get_apps().items():
+            apps[app] = {}
+            for url_name, form in forms.items():
+                known_url_names.remove(url_name)
+                apps[app][url_name] = form(url_name)
+                media += apps[app][url_name].media
+        for url in known_url_names:
+            apps['Other'][url] = None
+    context = {
+        'apps': apps,
+        'is_popup': request.GET.get('_popup', '') != '',
+        'media': media,
+    }
+    return render(request, "url_browser/app_list.html", context)
