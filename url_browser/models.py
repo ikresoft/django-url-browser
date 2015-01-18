0586b04b8814bdb82499145a6c2f73b3806edff0
Enver Bisevac <enver@Envers-MacBook-Pro.local>
2015-01-18 21:01:40 +0100

    fix

--- url_browser/models.py
+++ url_browser/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext as _
-from filer.utils.compatibility import python_2_unicode_compatible
+from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
